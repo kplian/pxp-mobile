2bb7b96e1480a32df527f54d72271b0963dbeee8
dora <dcastro@boa.bo>
2014-08-28 18:19:23 -0400

    compila codigo de testing

--- pxp/index.php
+++ pxp/index.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-session_set_cookie_params (0,'/', '' ,true ,false);
+session_set_cookie_params (0,'/', '' ,false ,false);
 if ($_SERVER['SERVER_PORT']!='443') { 
     // Fuerza SSL en esta página 
     //header("Location:https://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']); 
