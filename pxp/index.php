de0cebc26bc1cd4b7442900328156034dadd5419
dora <dcastro@boa.bo>
2014-08-28 18:21:41 -0400

    compila codigo de testing

--- pxp/index.php
+++ pxp/index.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-session_set_cookie_params (0,'/', '' ,false ,false);
+session_set_cookie_params (0,'/', '' ,true ,true);
 if ($_SERVER['SERVER_PORT']!='443') { 
     // Fuerza SSL en esta página 
     //header("Location:https://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']); 
--- rest/index.php
+++ rest/index.php
@@ -9,7 +9,7 @@ include 'PxpRestClient.php';
  * If you are using Composer, you can skip this step.
  */
 session_start();
-session_set_cookie_params (0,'/', '' ,false ,false);
+session_set_cookie_params (0,'/', '' ,true ,true);
 include 'config.php';
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
