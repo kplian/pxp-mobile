ba984e19033509445e9a9fc02143dd2c10e394ad
root <root@localhost.localdomain>
2014-09-12 18:25:18 -0400

    configuracion para puesta en production

---
+++ testing/pxp/rest/config.sample.php
@@ -0,0 +1,7 @@
+<?php
+    // Esta es la variable que indica el host del servidor Postgres
+	$_SESSION["_HOST"] = '172.17.45.229';
+	//Base URL ,  where is  the API  rest
+	$_SESSION["_URL_BASE"] = 'kerp_capacitacion/pxp/lib/rest/';
+
+?>
